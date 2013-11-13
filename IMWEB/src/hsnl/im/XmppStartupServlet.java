78436c78b36d674b7ff68c2ae9a4cb7059dbdcc9
dspp779 <dspp779@gmail.com>
2013-11-13 17:49:38 +0800

    XMPP startup servelet

---
+++ IMWEB/src/hsnl/im/XmppStartupServlet.java
@@ -0,0 +1,49 @@
+package hsnl.im;
+
+import hsnl.im.integrate.IntegratePool;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.security.util.Debug;
+
+/**
+ * Servlet implementation class XmppStartupServlet
+ */
+@WebServlet(description = "Startup XMPP client and login", urlPatterns = { "/XmppStartupServlet" })
+public class XmppStartupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public XmppStartupServlet() {
+        super();
+        try {
+			IntegratePool.init();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Debug.println("XMPP", "startup fail");
+		}
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
